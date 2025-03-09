86d106a8ead4d44f95e556258e079c9ae9daaad5
vill4in <j.adrian.pulido.s@gmail.com>
2025-03-08 20:27:48 -0600

    added: arduino as component refactor: main.c to main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,33 @@
+extern "C" {
+    #include "traction_control.h"
+    //#include "test_traction_control.h"
+    #include "kalman_filter.h"
+    #include "odometry_unit.h"
+    #include "differential_drive_ctrl.h"
+    #include "waypoint_controller.h"
+    #include "test_waypoint_follower.h"
+    //#include "seed_planter_control.h"
+    //#include "test_seed_planter.h"
+}
+
+#include "Arduino.h"
+
+extern "C" void app_main(void)
+{
+    initArduino();
+
+    traction_control_start_task();
+    odometry_unit_start_task();
+    kalman_filter_start_task();
+    diff_drive_ctrl_task_start();
+    waypoint_controller_start_task();
+
+    test_waypoint_follower_task_start();
+    //seed_planter_control_start_task();
+    //test_seed_planter_start_task();
+
+    for(;;)
+    {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
